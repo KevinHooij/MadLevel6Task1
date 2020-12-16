4ee7259dbe1c8331a7d1b8b50fc0808e57d96291
KevinHooij <kevin.hooijschuur@hva.nl>
2020-12-16 10:21:33 +0100

    Completes step 5

--- app/src/main/java/com/example/madlevel6task1/ColorFragment.kt
+++ app/src/main/java/com/example/madlevel6task1/ColorFragment.kt
@@ -5,11 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_color.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class ColorFragment : Fragment() {
+    private val colors = arrayListOf<ColorItem>()
+    private lateinit var colorAdapter: ColorAdapter
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -22,5 +28,15 @@ class ColorFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        colorAdapter = ColorAdapter(colors, ::onColorClick)
+        rvColors.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
+        rvColors.adapter = colorAdapter
+
+        //observeColors()
+    }
+
+
+    private fun onColorClick(colorItem: ColorItem) {
+        Snackbar.make(rvColors, "This color is: ${colorItem.name}", Snackbar.LENGTH_LONG).show()
     }
 }
